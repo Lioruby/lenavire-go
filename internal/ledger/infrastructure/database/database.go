0d9640f21a4c3c88cc0bda47e0c3aa598fe94d3f
Lior Levy <levy.lior.pro@gmail.com>
2025-03-19 04:01:23 +0100

    chore: fix database connexion for heroku

--- internal/ledger/infrastructure/database/database.go
+++ internal/ledger/infrastructure/database/database.go
@@ -12,21 +12,26 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
+	databaseUrl := os.Getenv("DATABASE_URL")
+	if databaseUrl == "" {
+		databaseUrl = fmt.Sprintf(
+			"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Paris",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
+			os.Getenv("DB_PORT"),
+		)
+	} else {
 
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Paris",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-	)
+		databaseUrl = databaseUrl + "?sslmode=require"
+	}
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(databaseUrl), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Erreur de connexion à la base de données:", err)
+		log.Fatal("Database connection failed:", err)
 	}
 
 	DB = db
-	fmt.Println("✅ Connexion à PostgreSQL réussie !")
+	fmt.Println("✅ Database connection successful!")
 }
